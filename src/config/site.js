097f920c0fe2f421965c7baf432e158d87794cd9
ImSoulRebel <chrystianmichell.as@gmail.com>
2025-10-11 00:53:57 +0200

    Initial commit

---
+++ src/config/site.js
@@ -0,0 +1,17 @@
+export const SITE_CONFIG = {
+  title: 'Chrystian Michell | Líder Técnico & Arquitecto Flutter',
+  description:
+    'Líder Técnico & Arquitecto Flutter especializado en desarrollo multiplataforma. Más de 5 años de experiencia, 10+ apps en producción en 11 países en Europa y LATAM.',
+  author: 'Chrystian Michell Álvarez Sánchez',
+  keywords:
+    'Flutter Developer, Dart, Tech Lead, Clean Architecture, BLoC Pattern, Desarrollo Multiplataforma, Apps Móviles, Smart TV, OTT Platform, Liderazgo Técnico',
+  url: 'https://chrystianmichell.dev',
+  image: '/og-image.jpg',
+  locale: 'es-ES',
+  twitter: '@chrystiandev',
+  linkedin: 'chrystianmichell',
+  github: 'chrystian-dev',
+  email: 'chrystianmichell@hotmail.com',
+  phone: '+34 653 31 69 44',
+  location: 'Aranjuez, Madrid, España',
+};
